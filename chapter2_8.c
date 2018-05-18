407aa70f4f10de55329a145caaff3add443a9b31
Stickce <35128383+Stickce@users.noreply.github.com>
2018-05-18 15:43:55 +0800

    chapter2_8.c

---
+++ chapter2_8.c
@@ -0,0 +1,22 @@
+/*chapter2_8.c*/
+#include <stdio.h>
+void one_three();
+void two();
+int main()
+{
+	printf("starting now:\n");
+	one_three();
+	printf("done!\n");
+	return 0;
+}
+void one_three()
+{
+	printf("one\n");
+	two();
+	printf("\n");
+	printf("three\n");
+}
+void two()
+{
+	printf("two");
+}
